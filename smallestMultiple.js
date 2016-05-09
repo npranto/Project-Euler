eadebac659766c117e3fd4017542a685bb14d8f9
Nazmuz Shakib Pranto <npranto@gmail.com>
2016-05-09 09:07:44 -0400

    Pro-Euler

---
+++ smallestMultiple.js
@@ -0,0 +1,23 @@
+// smallestMultiple() finds the smallest positive number that is evenly divisible by 
+// all of the numbers from fromNum1 to toNum2
+
+function smallestMultiple(fromNum1, toNum2) {
+	var currMultiple = 1;
+	var allWorks = false;
+	while(allWorks !== true){
+		var now = currMultiple;
+		for (var i = fromNum1; i <= toNum2; i++) {
+			if ((currMultiple % i) !== 0) {
+				console.log("DONE!");
+				allWorks = false;
+				currMultiple = currMultiple + 1;
+				break;
+			}
+		}
+		if (now === currMultiple) {
+			allWorks = true;
+		}
+	}
+	return currMultiple;
+}
+
