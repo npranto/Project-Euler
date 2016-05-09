45a28697248e12c58ef7d50c7036f60692479b4d
Nazmuz Shakib Pranto <npranto@gmail.com>
2016-05-09 16:50:02 -0400

    Pro-Euler

---
+++ sumSquareDifference.js
@@ -0,0 +1,21 @@
+// sumSquareDifference() function requires you to Find the difference between the sum 
+// of the squares of the first one hundred natural numbers and the square of the sum
+
+function sumSquareDifference(limit) {
+	var squareOfSum = 0;
+	var sumOfSquares = 0
+
+	// squareOfSum portion
+	for (var i = 1; i <= limit; i++) {
+		var curr = i*i;
+		sumOfSquares = sumOfSquares + curr;
+	}
+
+	// sumOfSquares portion
+	for (var i = 1; i <= limit; i++) {
+		squareOfSum = squareOfSum + i;
+	}
+	squareOfSum = squareOfSum * squareOfSum;
+
+	return squareOfSum - sumOfSquares;
+}
